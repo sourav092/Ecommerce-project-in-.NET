f51a3ad37a5587139b2b28e0fd5fdb6870106508
Sourav Choudhary <souravchoudhary092@gmail.com>
2024-07-29 18:38:49 +0530

    Add files via upload

---
+++ Company.js
@@ -0,0 +1,72 @@
+﻿var dataTable
+$(document).ready(function () {
+	loadDataTable();
+})
+function loadDataTable() {
+	dataTable = $('#tblData').DataTable({
+		"ajax": {
+			"url": "/Admin/Company/GetAll"
+		},
+		"columns": [
+
+			{ "data": "name", "width": "15%" },
+			{ "data": "streetAddress", "width": "15%" },
+			{ "data": "city", "width": "15%" },
+			{ "data": "state", "width": "15%" },
+			{ "data": "phoneNumber", "width": "15%" },
+			{
+				"data": "isAuthorizedCompany",
+				"render": function (data) {
+					if (data) {
+						return `<input type="checkbox" checked disable/>`;
+					}
+					else {
+						return `<input type="checkbox" disable/>`;
+					}
+				}
+			},
+			{
+				"data": "id",
+				"render": function (data) {
+					return `
+					<div class="text-center">
+					<a href="/Admin/Company/Upsert/${data}" class="btn btn-info">
+					<i class="fas fa-edit"></i>
+					</a>
+					<a class="btn btn-danger" onclick=Delete("/Admin/Company/Delete/${data}")>
+					<i class="fas fa-trash-alt"></i>
+					</a>
+					</div>
+					`;
+				}
+			}
+		]
+	})
+}
+function Delete(url) {
+	/*alert(url)*/
+	swal({
+		title: "want to delete data",
+		text: "Delete Information!!!!",
+		icon: "warning",
+		buttons: true,
+		dangerModel: true
+	}).then((willDelete) => {
+		if (willDelete) {
+			$.ajax({
+				url: url,
+				type: "Delete",
+				success: function (data) {
+					if (data.success) {
+						toastr.success(data.message);
+						dataTable.ajax.reload();
+					}
+					else {
+						toastr.error(data.message);
+					}
+				}
+			})
+		}
+	})
+
+}
---
+++ SD.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecomm_Project_11.Utility
+{
+    public static class SD
+    {
+        //cover type store procedure
+        public const string Proc_GetCoverTypes = "GetCoverTypes";
+        public const string Proc_GetCoverType = "GetCoverType";
+        public const string Proc_CreateCoverTypes = "CreateCoverType";
+        public const string Proc_UpdateCoverTypes = "UpdateCoverType";
+        public const string Proc_DeleteCoverTypes = "DeleteCoverType";
+        //Role
+        public const string Role_Admin = "Admin";
+        public const string Role_Employe = "Employee User";
+        public const string Role_Company = "Company User";
+        public const string Role_Individual = "Individual User";
+        //Session
+        public const string Ss_CartSessionCount = "Cart Count Session";
+        //****
+        public static double GetPriceBasedOnQuantity(double quantity,double price,double price50,double price100)
+        {
+            if (quantity < 50)
+                return price;
+            else if (quantity < 100)
+                return price50;
+            else return price100;
+        }
+        //Order Staus
+        public const string OrderStatusPending = "Pending";
+        public const string OrderStatusApproved = "Approved";
+        public const string OrderStatusInProgress = "Processing";
+        public const string OrderStatusShipped = "Shipped";
+        public const string OrderStatusCancelled = "Cancelled";
+        public const string OrderStatusRefunded = "Refunded";
+
+        //payment Status
+        public const string PaymentStatusPending = "Pending";
+        public const string PaymentStatusApproved = "Approved";
+        public const string PaymentStatusDelayPayment = "PaymentStatusDelay";
+        public const string PaymentStatusRejected = "Rejected";
+    }
+}
